204bb02fd311daab9eaf44b1877659c5740c8635
sam82520.work <sam82520.work@gmail.com>
2020-02-25 14:14:59 +0800

    create marker-shadow.png

--- src/components/marker.js
+++ src/components/marker.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Marker /*, Popup*/ } from "react-leaflet";
 import L from "leaflet";
-import styled from "styled-components";
 
 export default props => {
   const { location } = props.item;
   const iconPublicURL = props.iconPublicURL;
+  const shadowPublicURL = props.shadowPublicURL;
 
   const icon = L.icon({
     iconUrl: iconPublicURL,
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    shadowUrl: shadowPublicURL,
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
@@ -19,5 +18,3 @@ export default props => {
 
   return <Marker icon={icon} position={location} />;
 };
-
-const Img = styled.img``;
