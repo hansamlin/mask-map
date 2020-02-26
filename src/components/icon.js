4b7092954545544bc3de50fb3c07e2d5bdb874a1
sam82520.work <sam82520.work@gmail.com>
2020-02-26 11:36:22 +0800

    封裝 createIcon

---
+++ src/components/icon.js
@@ -0,0 +1,20 @@
+import L from "leaflet";
+
+export const createIcon = ({ iconUrl, shadowUrl }) =>
+  L.icon({
+    iconUrl,
+    shadowUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+export const getIconUrl = source => {
+  const [shadow] = source.filter(item => item.name === "marker-shadow");
+  const [orange] = source.filter(item => item.name === "marker-icon-orange");
+  const [red] = source.filter(item => item.name === "marker-icon-red");
+  const [green] = source.filter(item => item.name === "marker-icon-green");
+
+  return { shadow, orange, red, green };
+};
