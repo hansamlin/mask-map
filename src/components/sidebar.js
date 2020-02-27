8d0a34e2b5eafdb89af54767384a55bdd2ca2989
Sam <sam82520.work@gmail.com>
2020-02-27 23:18:44 +0800

    resolve click sidebar can toggle sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 
 import Info from "./info";
 import Search from "./search";
@@ -12,13 +12,16 @@ export default () => {
   const handleToggle = () => setToggle(prev => !prev);
 
   return (
-    <Container theme={{ toggle }} onClick={handleToggle}>
-      <Info />
-      <TextProvider>
-        <Search />
-        <List />
-      </TextProvider>
-    </Container>
+    <ThemeProvider theme={{ toggle }}>
+      <Container>
+        <Info />
+        <TextProvider>
+          <Search />
+          <List />
+        </TextProvider>
+        <Toggle onClick={handleToggle}></Toggle>
+      </Container>
+    </ThemeProvider>
   );
 };
 
@@ -37,22 +40,6 @@ const Container = styled.div`
       props.theme.toggle ? "translateX(-100%)" : "translateX(0)"};
     position: fixed;
     transition: transform 0.5s;
-
-    &::after {
-      content: "";
-      position: absolute;
-      width: 25px;
-      height: 50px;
-      right: -25px;
-      top: 10vh;
-      cursor: pointer;
-      background: rgba(255, 255, 255, 0.9)
-        url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAQAAAAXDMSnAAAAi0lEQVR4AX3JQcqBURQG4O/+9WNG30D3vOfSDTuQsgcZyBakZANSzMVMBme3zsBI5/VMn4ZKLP5ki1E4tYejWpilxVUtzOEUD68odYmXR5BJNp/4zllXD2phllYvamHmirsayUkfJ5ruHzueTldC08kcT5YOY9xYujqQM03XKXuaLmEtNF1e1Nz89gbL+0do6OEwRwAAAABJRU5ErkJggg==)
-        7px center/7px 10px no-repeat;
-      box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.3);
-      z-index: -1;
-      transform: ${props => props.theme.toggle && "scaleX(-1)"};
-    }
   }
 
   @media (min-width: 768px) {
@@ -63,4 +50,20 @@ const Container = styled.div`
       display: none;
     }
   }
+`;
+
+const Toggle = styled.button`
+  position: absolute;
+  width: 25px;
+  height: 50px;
+  right: -25px;
+  top: 10vh;
+  cursor: pointer;
+  background: rgba(255, 255, 255, 0.9)
+    url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAQAAAAXDMSnAAAAi0lEQVR4AX3JQcqBURQG4O/+9WNG30D3vOfSDTuQsgcZyBakZANSzMVMBme3zsBI5/VMn4ZKLP5ki1E4tYejWpilxVUtzOEUD68odYmXR5BJNp/4zllXD2phllYvamHmirsayUkfJ5ruHzueTldC08kcT5YOY9xYujqQM03XKXuaLmEtNF1e1Nz89gbL+0do6OEwRwAAAABJRU5ErkJggg==)
+    7px center/7px 10px no-repeat;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.3);
+  z-index: -1;
+  transform: ${props => props.theme.toggle && "scaleX(-1)"};
+  outline: none;
 `;
