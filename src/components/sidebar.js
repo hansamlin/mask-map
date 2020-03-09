7d0e40d8ae6df016ff14ef75cd679fdc9b0cfbdf
Sam <sam82520.work@gmail.com>
2020-03-09 22:36:03 +0800

    adjust sidebar button width

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -50,14 +50,16 @@ const Container = styled.div`
 
 const Toggle = styled.button`
   position: absolute;
-  width: 25px;
+  width: 30px;
   height: 50px;
-  right: -25px;
+  right: -30px;
   top: 10vh;
   cursor: pointer;
   background: rgba(255, 255, 255, 0.9)
     url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAQAAAAXDMSnAAAAi0lEQVR4AX3JQcqBURQG4O/+9WNG30D3vOfSDTuQsgcZyBakZANSzMVMBme3zsBI5/VMn4ZKLP5ki1E4tYejWpilxVUtzOEUD68odYmXR5BJNp/4zllXD2phllYvamHmirsayUkfJ5ruHzueTldC08kcT5YOY9xYujqQM03XKXuaLmEtNF1e1Nz89gbL+0do6OEwRwAAAABJRU5ErkJggg==)
-    7px center/7px 10px no-repeat;
+    no-repeat;
+    background-position: center;
+    background-size: 10px;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.3);
   z-index: -1;
   transform: ${props => props.theme.toggle && "scaleX(-1)"};
